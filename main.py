66e107a63988414d3f0a77b24eb47b47cf1c611a
Eugene <banbelov@yandex.ru>
2024-10-04 22:31:33 +0300

    unsync update working

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import time
 from PyQt6 import uic
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6 import QtCore
 
 # иницилизация порта Ардуино
 ser = serial.Serial()
 ser.baudrate = 9600
-ser.port = "COM6"
+ser.port = "COM3"
 ser.open()
 time.sleep(2)
+
+# возвращаемая строка ардуино
+astring = ""
 
 
 # helper function for running a target periodically
@@ -30,7 +34,6 @@ async def fun1():
     time.sleep(0.1)
     while ser.in_waiting:
         return ser.readline().decode("utf-8")[:-1]
-    ser.close()
 
 
 # функция, которая ничего не делает
@@ -40,7 +43,7 @@ async def fun2():
     return "2"
 
 
-# функция, которая загружает интерфейс
+# функция, которая ничего не делает
 async def fun3():
     await asyncio.sleep(1)
     print("функция 3")
@@ -56,7 +59,6 @@ async def main():
     # эта функция срабатывает при нажатии кнопки
     def btn_clk():
         print("Button clicked")
-
         form.lcdNumber.display(form.lcdNumber.intValue() + 1)
 
     def ln_changed():
@@ -64,7 +66,11 @@ async def main():
         form.lcdNumber_2.display(2)
 
     def lbl3_add():
-        form.label_3.setText(task1.result())
+        time.sleep(0.5)
+        ser.write(b"1")
+        time.sleep(0.1)
+        while ser.in_waiting:
+            form.label_3.setText(ser.readline().decode("utf-8")[:-1])
 
     # подключаем файл, полученный в QtDesigner
     Form, Window = uic.loadUiType("interface.ui")
@@ -77,8 +83,9 @@ async def main():
     form.pushButton.clicked.connect(lbl3_add)
     form.lineEdit.textChanged.connect(ln_changed)
 
-    await task1
-    form.label_3.setText(task1.result())
+    timer1 = QtCore.QTimer()  # set up your QTimer
+    timer1.timeout.connect(lbl3_add)  # connect it to your update function
+    timer1.start(1000)  # set it to timeout in 5000 ms
 
     # запускаем окно программы
     app.exec()
